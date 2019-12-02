77cd26460e57b2591c84b023a301afcec5f59cc1
Guilherme Pontes <guilhermepontes@msn.com>
2019-12-02 17:31:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const terminalImage = require('terminal-image')
 
 (async () => {
-    console.log(await terminalImage('profile.jpg', {
+  console.log(await terminalImage('profile.jpg', {
     width: 10,
     preserveAspectRatio: true,
   }))
